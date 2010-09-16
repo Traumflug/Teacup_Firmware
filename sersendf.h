256ecdca3909eec1c8e90341e2e353a08e678f7f
Michael Moon <triffid.hunter@gmail.com>
2010-09-16 22:08:24 +1000

    compile-time format checking for sersendf_P

--- sersendf.h
+++ sersendf.h
@@ -4,6 +4,6 @@
 #include	<avr/pgmspace.h>
 
 void sersendf(char *format, ...)		__attribute__ ((format (printf, 1, 2)));
-void sersendf_P(PGM_P format, ...);
+void sersendf_P(PGM_P format, ...)	__attribute__ ((format (printf, 1, 2)));
 
 #endif	/* _SERSENDF_H */
