4bddf3452fd0a9ab2e060b202d295aa3fc3990a6
Markus Hitter <mah@jump-ing.de>
2010-10-07 20:24:07 +0200

    Move stepper macros from config.h(.dist) to dda.c, as they're not meant to be configured by the user.

--- dda.c
+++ dda.c
@@ -10,6 +10,67 @@
 #include	"debug.h"
 #include	"sersendf.h"
 
+/*
+	X Stepper
+*/
+
+#define	_x_step(st)						WRITE(X_STEP_PIN, st)
+#define	x_step()							_x_step(1);
+#define	x_direction(dir)			WRITE(X_DIR_PIN, dir)
+#define	x_min()								READ(X_MIN_PIN)
+#ifdef	X_MAX_PIN
+	#define	x_max()							READ(X_MAX_PIN)
+#else
+	#define	x_max()							(0)
+#endif
+
+/*
+	Y Stepper
+*/
+
+#define	_y_step(st)						WRITE(Y_STEP_PIN, st)
+#define	y_step()							_y_step(1);
+#define	y_direction(dir)			WRITE(Y_DIR_PIN, dir)
+#define	y_min()								READ(Y_MIN_PIN)
+#ifdef	Y_MAX_PIN
+	#define	y_max()							READ(Y_MAX_PIN)
+#else
+	#define	y_max()							(0)
+#endif
+
+/*
+	Z Stepper
+*/
+
+#define	_z_step(st)						WRITE(Z_STEP_PIN, st)
+#define	z_step()							_z_step(1);
+#define	z_direction(dir)			WRITE(Z_DIR_PIN, dir)
+#define	z_min()								READ(Z_MIN_PIN)
+#ifdef	Z_MAX_PIN
+	#define	z_max()							READ(Z_MAX_PIN)
+#else
+	#define	z_max()							(0)
+#endif
+
+/*
+	Extruder
+*/
+
+#define	_e_step(st)						WRITE(E_STEP_PIN, st)
+#define	e_step()							_e_step(1);
+#define	e_direction(dir)			WRITE(E_DIR_PIN, dir)
+
+/*
+	End Step - All Steppers
+	(so we don't have to delay in interrupt context)
+*/
+
+#define unstep() 							do { _x_step(0); _y_step(0); _z_step(0); _e_step(0); } while (0)
+
+/*
+	Maths
+*/
+
 #ifndef	ABS
 #define	ABS(v)		(((v) >= 0)?(v):(-(v)))
 #endif
