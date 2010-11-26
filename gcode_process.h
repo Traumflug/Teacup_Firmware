29391e64e7fa44197fba1a49868d99eda49f45c1
Michael Moon <triffid.hunter@gmail.com>
2010-11-27 10:30:12 +1100

    add new function prototypes to gcode_process.h

--- gcode_process.h
+++ gcode_process.h
@@ -3,9 +3,13 @@
 
 #include	"gcode_parse.h"
 
+
+void zero_x(void);
+void zero_y(void);
+void zero_z(void);
+void zero_e(void);
+
 // this is where we construct a move without a gcode command, useful for gcodes which require multiple moves eg; homing
-void SpecialMoveXY(int32_t x, int32_t y, uint32_t f);
-void SpecialMoveZ(int32_t z, uint32_t f);
 void SpecialMoveE(int32_t e, uint32_t f);
 
 // when we have a whole line, feed it to this
