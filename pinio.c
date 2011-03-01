c182cc89298f596cb8cccdb79070af0069724dac
Markus Hitter <mah@jump-ing.de>
2011-03-01 21:34:38 +0100

    pinio.c: remove duplicate #ifdef wrapping.
    In case of no enable pins, these functions resolve to nothing
    anyways, see pinio.h.

--- pinio.c
+++ pinio.c
@@ -1,18 +1,10 @@
 #include	"pinio.h"
 
 void power_off() {
-	#ifdef	X_ENABLE_PIN
-		x_disable();
-	#endif
-	#ifdef	Y_ENABLE_PIN
-		y_disable();
-	#endif
-	#ifdef	Z_ENABLE_PIN
-		z_disable();
-	#endif
-	#ifdef	E_ENABLE_PIN
-		e_disable();
-	#endif
+
+	x_disable();
+	y_disable();
+	z_disable();
 
 	#ifdef	STEPPER_ENABLE_PIN
 	WRITE(STEPPER_ENABLE_PIN, STEPPER_ENABLE_INVERT ^ 1);
