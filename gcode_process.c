3e32a8f634cb5f5c2ea8e69130d716acfcd82665
Michael Moon <triffid.hunter@gmail.com>
2011-05-01 17:17:56 +1000

    allow M114 to report negative positions correctly

--- gcode_process.c
+++ gcode_process.c
@@ -393,7 +393,7 @@ void process_gcode_command() {
 				// M113- extruder PWM
 			// M114- report XYZEF to host
 			case 114:
-				sersendf_P(PSTR("X:%lq,Y:%lq,Z:%lq,E:%lq,F:%ld"), current_position.X * UM_PER_STEP_X, current_position.Y * UM_PER_STEP_Y, current_position.Z * UM_PER_STEP_Z, current_position.E * UM_PER_STEP_E, current_position.F);
+				sersendf_P(PSTR("X:%lq,Y:%lq,Z:%lq,E:%lq,F:%ld"), current_position.X * ((int32_t) UM_PER_STEP_X), current_position.Y * ((int32_t) UM_PER_STEP_Y), current_position.Z * ((int32_t) UM_PER_STEP_Z), current_position.E * ((int32_t) UM_PER_STEP_E), current_position.F);
 				// newline is sent from gcode_parse after we return
 				break;
 			// M115- capabilities string
