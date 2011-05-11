5a6855f046a7ddb8fa3a9530f28f1fbca550f60d
Jim McGee <madscifi@jetblackcat.com>
2011-05-10 22:19:45 -0700

    Clean up delay functions and rename them to be consistent with what was used before the inline delay approach was attempted.

--- delay.c
+++ delay.c
@@ -4,20 +4,33 @@
 	\brief Delay routines
 */
 
-#include <util/delay_basic.h>
+//#include <util/delay_basic.h>
 
-#include	"watchdog.h"
+//#include	"watchdog.h"
+
+
+/// interruptable microsecond delay
+/// does NOT call wd_reset
+/// \param delay time in microseconds
+void delay_us( uint16_t delay )
+{
+	while (delay > (65536UL / (F_CPU / 4000000UL))) {
+		_delay_loop_2(65534); //
+		delay -= (65536L / (F_CPU / 4000000L));
+	}
+	_delay_loop_2( delay * (F_CPU / 4000000UL));
+}
 
 /// delay microseconds
 /// \param delay time to wait in microseconds
-void delay_us(uint16_t delay) {
+void _delay(uint32_t delay) {
 	wd_reset();
 	while (delay > (65536L / (F_CPU / 4000000L))) {
 		_delay_loop_2(65534); // we use 65534 here to compensate for the time that the surrounding loop takes. TODO: exact figure needs tuning
 		delay -= (65536L / (F_CPU / 4000000L));
 		wd_reset();
 	}
-	_delay_loop_2(delay / (F_CPU / 4000000L));
+	_delay_loop_2(delay * (F_CPU / 4000000L));
 	wd_reset();
 }
 
