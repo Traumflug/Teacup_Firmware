4f748c68b1f2ea370db0c928515e1f50a60b3819
Markus Hitter <mah@jump-ing.de>
2011-08-15 02:18:58 +0200

    Bring config.gen7.h up do date.
    There was a recent rename which wasn't in the contributed example yet.

--- config.gen7.h
+++ config.gen7.h
@@ -86,7 +86,6 @@
 // no SEARCH_FEEDRATE_E, as E can't be searched
 
 /// this is how many steps to suck back the filament by when we stop. set to zero to disable
-//#define	E_STARTSTOP_STEPS		0
 #define	E_STARTSTOP_STEPS			289
 
 /**
@@ -109,7 +108,7 @@
 /**	\def E_ABSOLUTE
 	Some G-Code creators produce relative length commands for the extruder, others absolute ones. G-Code using absolute lengths can be recognized when there are G92 E0 commands from time to time. If you have G92 E0 in your G-Code, define this flag.
 */
-//#define E_ABSOLUTE
+// #define E_ABSOLUTE
 
 
 
@@ -182,44 +181,43 @@
 */
 
 #define	X_STEP_PIN						DIO19
-#define	X_DIR_PIN						DIO18
-#define	X_MIN_PIN						DIO7
-//#define	X_MAX_PIN
-//#define	X_ENABLE_PIN
+#define	X_DIR_PIN							DIO18
+#define	X_MIN_PIN							DIO7
+//#define	X_MAX_PIN							xxxx
+//#define	X_ENABLE_PIN					xxxx
 //#define	X_INVERT_DIR
 //#define	X_INVERT_MIN
 //#define	X_INVERT_MAX
 //#define	X_INVERT_ENABLE
 
 #define	Y_STEP_PIN						DIO23
-#define	Y_DIR_PIN						DIO22
-#define	Y_MIN_PIN						DIO5
-//#define	Y_MAX_PIN
-//#define	Y_ENABLE_PIN
+#define	Y_DIR_PIN							DIO22
+#define	Y_MIN_PIN							DIO5
+//#define	Y_MAX_PIN							xxxx
+//#define	Y_ENABLE_PIN					xxxx
 //#define	Y_INVERT_DIR
 //#define	Y_INVERT_MIN
 //#define	Y_INVERT_MAX
 //#define	Y_INVERT_ENABLE
 
 #define	Z_STEP_PIN						DIO26
-#define	Z_DIR_PIN						DIO25
-#define	Z_MIN_PIN						DIO1
-//#define	Z_MAX_PIN
-//#define	Z_ENABLE_PIN
+#define	Z_DIR_PIN							DIO25
+#define	Z_MIN_PIN							DIO1
+//#define	Z_MAX_PIN							xxxx
+//#define	Z_ENABLE_PIN					xxxx
 //#define	Z_INVERT_DIR
 //#define	Z_INVERT_MIN
 //#define	Z_INVERT_MAX
 //#define	Z_INVERT_ENABLE
 
 #define	E_STEP_PIN						DIO28
-#define	E_DIR_PIN						DIO27
+#define	E_DIR_PIN							DIO27
 //#define E_ENABLE_PIN
 //#define	E_INVERT_DIR
 
-
-#define STEPPER_ENABLE_PIN					DIO24
-#define	STEPPER_ENABLE_INVERT
-#define	PS_ON_PIN						DIO15
+#define	PS_ON_PIN							DIO15
+#define STEPPER_ENABLE_PIN		DIO24
+#define	STEPPER_INVERT_ENABLE
 
 
 
