5a9a134ae46ec886c8cc02d00976769c269f7b53
Markus Hitter <mah@jump-ing.de>
2012-03-06 12:08:43 +0100

    gcode_parse.c: review decfloat_to_int() yet again.
    This time, raise range at the cost of some precision for the
    newly explored areas.

--- gcode_parse.c
+++ gcode_parse.c
@@ -57,9 +57,9 @@ extern const uint32_t powers[];  // defined in sermsg.c
 /// convert a floating point input value into an integer with appropriate scaling.
 /// \param *df pointer to floating point structure that holds fp value to convert
 /// \param multiplicand multiply by this amount during conversion to integer
-/// \param divide_by_1000 divide by 1000 during conversion to integer
 ///
-/// lots of work has been done in exploring this function's limitations in terms of overflow and rounding
+/// Tested for up to 42'000 mm (accurate), 420'000 mm (precision 10 um) and
+/// 4'200'000 mm (precision 100 um).
 static int32_t decfloat_to_int(decfloat *df, uint32_t multiplicand) {
 	uint32_t	r = df->mantissa;
 	uint8_t	e = df->exponent;
@@ -67,6 +67,13 @@ static int32_t decfloat_to_int(decfloat *df, uint32_t multiplicand) {
 	// e=1 means we've seen a decimal point but no digits after it, and e=2 means we've seen a decimal point with one digit so it's too high by one if not zero
 	if (e)
 		e--;
+
+	// This raises range for mm by factor 1000 and for inches by factor 100.
+	// It's a bit expensive, but we should have the time while parsing.
+	while (e && multiplicand % 10 == 0) {
+		multiplicand /= 10;
+		e--;
+	}
 
 	r *= multiplicand;
 	if (e)
