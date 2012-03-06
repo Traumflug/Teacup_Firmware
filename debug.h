b8ab255b6b651b270cdaa4c2fa483fc3306094aa
Markus Hitter <mah@jump-ing.de>
2012-03-06 12:45:08 +0100

    Sort DEBUG_ECHO properly.
    Saves 200 bytes.

--- debug.h
+++ debug.h
@@ -7,14 +7,15 @@
 	#define		DEBUG_PID				1
 	#define		DEBUG_DDA				2
 	#define		DEBUG_POSITION	4
+	#define		DEBUG_ECHO			128
 #else
 	// by setting these to zero, the compiler should optimise the relevant code out
 	#define		DEBUG_PID				0
 	#define		DEBUG_DDA				0
 	#define		DEBUG_POSITION	0
+	#define		DEBUG_ECHO			0
 #endif
 
-#define		DEBUG_ECHO			128
 
 extern volatile uint8_t	debug_flags;
 
