0068ed1e2692a59631c1784fac99327581310418
Markus Hitter <mah@jump-ing.de>
2012-04-24 03:30:57 +0200

    Use the new muldiv() to replace um_to_steps..().
    This gets rid of overflows at micrometer to step conversion as
    much as possible within 31 bits. It also opens the door to get
    STEPS_PER_M configurable at runtime.
    
    This also costs 290 bytes, unfortunately.

--- dda_maths.h
+++ dda_maths.h
@@ -3,9 +3,16 @@
 
 #include	<stdint.h>
 
+#include	"config.h"
 
 // return rounded result of multiplicand * multiplier / divisor
 const int32_t muldiv(int32_t multiplicand, uint32_t multiplier,
                      uint32_t divisor);
+
+// convert micrometer distances to motor step distances
+#define um_to_steps_x(distance) muldiv(distance, STEPS_PER_M_X, 1000000UL);
+#define um_to_steps_y(distance) muldiv(distance, STEPS_PER_M_Y, 1000000UL);
+#define um_to_steps_z(distance) muldiv(distance, STEPS_PER_M_Z, 1000000UL);
+#define um_to_steps_e(distance) muldiv(distance, STEPS_PER_M_E, 1000000UL);
 
 #endif	/* _DDA_MATHS_H */
