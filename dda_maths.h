d81da50f4226d21d54d3ff8bf401f91ed1877b9b
Markus Hitter <mah@jump-ing.de>
2012-06-23 14:23:12 +0200

    Make muldiv() multiplicand signed.
    As Andrey correctly pointed out, we need this signed. See:
    http://forums.reprap.org/read.php?147,89710,130225#msg-130225
    and following posts.

--- dda_maths.h
+++ dda_maths.h
@@ -5,7 +5,7 @@
 
 
 // return rounded result of multiplicand * multiplier / divisor
-const uint32_t muldiv(uint32_t multiplicand, uint32_t multiplier,
-                      uint32_t divisor);
+const int32_t muldiv(int32_t multiplicand, uint32_t multiplier,
+                     uint32_t divisor);
 
 #endif	/* _DDA_MATHS_H */
