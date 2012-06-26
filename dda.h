81f85b018d4dde6dca3aa85c8ff1e2f743df9c71
Markus Hitter <mah@jump-ing.de>
2012-06-27 01:45:41 +0200

    Move utility functions from dda.c to dda_math.c.
    Simple cleanup, no functional change.

--- dda.h
+++ dda.h
@@ -159,12 +159,6 @@ extern TARGET current_position;
 	methods
 */
 
-uint32_t approx_distance( uint32_t dx, uint32_t dy )								__attribute__ ((hot));
-uint32_t approx_distance_3( uint32_t dx, uint32_t dy, uint32_t dz )	__attribute__ ((hot));
-
-// const because return value is always the same given the same v
-const uint8_t	msbloc (uint32_t v)																		__attribute__ ((const));
-
 // initialize dda structures
 void dda_init(void);
 
