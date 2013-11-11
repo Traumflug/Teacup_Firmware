cf015623e3439bbe5247a7f2fb63220af16db0b7
Phil Hord <hordp@cisco.com>
2013-11-11 13:32:58 -0500

    simulator: Add position to datalog.out
    Cleanup datalog output a bit.
      * Add close/flush on exit in case we have pending data
      * Use hash for comment characters to be compatible with gnuplot
      * Report x/y/z/e position in array

--- simulator/data_recorder.c
+++ simulator/data_recorder.c
@@ -8,13 +8,22 @@
 #include "simulator.h"
 #include "data_recorder.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 static FILE *file;
 
 #define MAX_PINS 100
-static uint32_t values[MAX_PINS];      ///< Pin and value states
+static int32_t values[MAX_PINS];       ///< Pin and value states
 static int pin_count;                  ///< Number of pins we emit
+static void emit_log_data(void);
+
+static void recorder_close(int code, void*x ) {
+  // force last line to emit
+  emit_log_data();
+  fflush(file);
+  fclose(file);
+}
 
 void recorder_init(const char* filename) {
   sim_assert( ! file, "Recorder already initialized");
@@ -23,36 +32,45 @@ void recorder_init(const char* filename) {
   sim_assert(file, "record_init: failed to create file");
 
   time_t t = time(NULL);
-  fprintf(file, "; Teacup_Firmware simulator data log on %s\n",
-          asctime(localtime(&t)));
+  fprintf(file, "# Teacup_Firmware simulator v1.0\n");
+  fprintf(file, "# Recorded %s\n", asctime(localtime(&t)));
   fflush(file);
+  on_exit(recorder_close, NULL);
 }
 
 void add_trace_var(const char* name, int pin) {
   sim_assert(file, "add_trace_var: Recorder not initialized");
   sim_assert(pin < MAX_PINS, "pin number invalid");
 
-  fprintf(file, "; %d - %s\n", pin, name);
+  fprintf(file, "# %d - %s\n", pin, name);
   if (pin >= pin_count)
     pin_count = pin + 1;
   fflush(file);
 }
 
-// Record a binary signal change
-void record_pin(int pin, bool state, uint32_t t) {
-  if (state == values[pin]) return;
-
-  sim_assert(file , "record_pin: Recorder not initialized");
-  sim_assert(pin < MAX_PINS, "pin number invalid");
-
-  values[pin] = state;
-
+static uint64_t prev_t;
+static void emit_log_data(void) {
   // Naive format: each line contains all values, beginning with the time
-  fprintf(file, "%u", t);
+  fprintf(file, "%lu", prev_t/1000); // microseconds
   for (int i = 0; i < pin_count; i++)
     fprintf(file, " %u", values[i]);
   fprintf(file, "\n");
   fflush(file);
+}
+
+// Record a value signal change
+void record_pin(int pin, int32_t state, uint64_t t) {
+  if (state == values[pin] && t == prev_t ) return;
+
+  sim_assert(file , "record_pin: Recorder not initialized");
+  sim_assert(pin < MAX_PINS, "pin number invalid");
+
+  // Record previous state when new state value appears
+  if ( t != prev_t ) {
+    emit_log_data();
+  }
+  prev_t = t;
+  values[pin] = state;
 }
 
 static char comment_buffer[300];
@@ -75,6 +93,6 @@ void record_comment_stream(char ch) {
 }
 
 void record_comment(const char * msg) {
-  fprintf(file, "; %s\n", msg);
+  fprintf(file, "# %s\n", msg);
   fflush(file);
 }
