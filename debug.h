32481e2799e34b73d866848208fed836e2a450ee
David Forrest <drf@vims.edu>
2014-06-17 20:10:30 -0400

    debug.h: Align M111 debug bit codes with Repetier-Host.
    No code changes, binary size and performance kept.

--- debug.h
+++ debug.h
@@ -4,16 +4,21 @@
 #include	<stdint.h>
 
 #ifdef	DEBUG
-	#define		DEBUG_PID				1
-	#define		DEBUG_DDA				2
-	#define		DEBUG_POSITION	4
-	#define		DEBUG_ECHO			128
+  #define DEBUG_ECHO       1
+  #define DEBUG_INFO       2
+  #define DEBUG_ERRORS     4
+  #define DEBUG_DRYRUN     8
+  #define DEBUG_PID       16
+  #define DEBUG_DDA       32
+  #define DEBUG_POSITION  64
 #else
 	// by setting these to zero, the compiler should optimise the relevant code out
 	#define		DEBUG_PID				0
 	#define		DEBUG_DDA				0
 	#define		DEBUG_POSITION	0
 	#define		DEBUG_ECHO			0
+  #define DEBUG_INFO       0
+  #define DEBUG_DRYRUN     0
 #endif
 
 
