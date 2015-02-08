face7c0dc7ff7ba0050d359ee8475ef4cc8782bf
jbernardis <jeff.bernardis@gmail.com>
2015-02-07 23:38:12 -0500

    Configtool: convert template for Gen3.

--- config.default.h
+++ config.default.h
@@ -7,6 +7,7 @@
 */
 
 // Uncomment your controller board, comment out all others.
+//#include "config/board.gen3.h"
 //#include "config/board.gen7-v1.1-v1.3.h"
 #include "config/board.gen7-v1.4.h"
 //#include "config/board.ramps-v1.2.h"
