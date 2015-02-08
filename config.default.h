31858b70820286e63125d2f3223cb9a573be5620
jbernardis <jeff.bernardis@gmail.com>
2015-02-07 23:38:12 -0500

    Configtool: convert template for Gen7 v1.1-v1.3.
    Also sort printers in config.default.h alphabetically and make
    Mendel the default printer.

--- config.default.h
+++ config.default.h
@@ -7,9 +7,10 @@
 */
 
 // Uncomment your controller board, comment out all others.
+//#include "config/board.gen7-v1.1-v1.3.h"
 #include "config/board.gen7-v1.4.h"
 //#include "config/board.ramps-v1.3.h"
 
 // Uncomment your printer type, comment out all others.
-#include "config/printer.wolfstrap.h"
-//#include "config/printer.mendel.h"
+#include "config/printer.mendel.h"
+//#include "config/printer.wolfstrap.h"
--- configtool/protectedfiles.py
+++ configtool/protectedfiles.py
@@ -1,5 +1,6 @@
 
 protectedFiles = [
+  "board.gen7-v1.1-v1.3.h",
   "board.gen7-v1.4.h",
   "board.ramps-v1.3.h",
   "printer.mendel.h",
