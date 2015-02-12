b6cd9fe63a2efd979d6d34609aadc923a79ba16d
NickE37 <bizman1001@yahoo.com>
2015-02-12 07:35:08 -0600

    Forgot this when moving to github

--- dda.h
+++ dda.h
@@ -17,6 +17,16 @@
   #define PROGMEM
 #endif
 
+#ifdef DELTA_PRINTER
+extern const int32_t delta_tower1_x,delta_tower1_y;
+extern const int32_t delta_tower2_x,delta_tower2_y;
+extern const int32_t delta_tower3_x,delta_tower3_y;
+extern const uint32_t DELTA_DIAGONAL_ROD_2;
+extern int32_t endstop_adj_x,endstop_adj_y,endstop_adj_z;
+extern int32_t delta_height;
+extern uint8_t bypass_delta;
+#endif
+
 /*
 	types
 */
