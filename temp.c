58d0ad98a9174915e24df6c643505cf8d49bd4ac
NickE37 <bizman1001@yahoo.com>
2015-02-13 22:38:19 -0600

    Add conditional compilation to LCD functions

--- temp.c
+++ temp.c
@@ -377,6 +377,7 @@ static void single_temp_print(temp_sensor_t index) {
   #endif
 }
 
+#ifdef LCD
 static void single_temp_lcd(temp_sensor_t index) {
 	uint8_t c = (temp_sensors_runtime[index].last_read_temp & 3) * 25;
 	lcdsendf_P(PSTR("%u"), temp_sensors_runtime[index].last_read_temp >> 2);
@@ -386,6 +387,7 @@ static void single_temp_lcd(temp_sensor_t index) {
     lcdsendf_P(PSTR("%u"), temp_sensors_runtime[index].target_temp >> 2);
   #endif
 }
+#endif
 
 /// send temperatures to host
 /// \param index sensor value to send
@@ -409,6 +411,7 @@ void temp_print(temp_sensor_t index) {
 	}
 }
 
+#ifdef LCD
 void temp_lcd(temp_sensor_t index) {
 
 	if (index == TEMP_SENSOR_none) { // standard behaviour
@@ -428,4 +431,5 @@ void temp_lcd(temp_sensor_t index) {
 		single_temp_lcd(index);
 	}
 }
-#endif
+#endif //LCD
+#endif //ifndef EXTRUDER
