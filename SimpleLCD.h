40463666cae1c7fd3cbd3ddb23b07f59d36076ed
NickE37 <bizman1001@yahoo.com>
2015-02-19 12:53:34 -0600

    Add custom characters to LCD support

--- SimpleLCD.h
+++ SimpleLCD.h
@@ -10,6 +10,17 @@
 #ifndef SimpleLCD_h
 #define SimpleLCD_h
 
+/* Custom characters defined in the first 8 characters of the LCD
+Lifted directly from ultralcd_implementation_hitachi_HD44780.h in Marlin*/
+#define LCD_STR_BEDTEMP     "\x00"
+#define LCD_STR_DEGREE      "\x01"
+#define LCD_STR_THERMOMETER "\x02"
+#define LCD_STR_UPLEVEL     "\x03"
+#define LCD_STR_REFRESH     "\x04"
+#define LCD_STR_FOLDER      "\x05"
+#define LCD_STR_FEEDRATE    "\x06"
+#define LCD_STR_CLOCK       "\x07"
+#define LCD_STR_ARROW_RIGHT "\x7E"  /* from the default character set */
 
 void lcdGoToXY(uint8_t x, uint8_t y);
 void lcdGoToAddr(uint8_t addr);
@@ -20,5 +31,6 @@ void lcdWriteChar(char *text);
 void lcdBusy();
 void sendCommand(uint8_t opCode);
 void sendCommand4Bit(uint8_t opCode);
+void lcdCreateChar(uint8_t location, uint8_t charmap[]);
 
 #endif
