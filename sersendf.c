5b8068d228be020343a9b5adf11abb8864eb316c
Markus Hitter <mah@jump-ing.de>
2015-07-22 15:01:34 +0200

    sersendf.c/.h: remove sersendf() (not sersendf_P()).
    Was commented out already and probably never used.

--- sersendf.c
+++ sersendf.c
@@ -9,66 +9,6 @@
 #include	"serial.h"
 #include	"sermsg.h"
 
-// void sersendf(char *format, ...) {
-// 	va_list args;
-// 	va_start(args, format);
-//
-// 	uint16_t i = 0;
-// 	uint8_t c, j = 0;
-// 	while ((c = format[i++])) {
-// 		if (j) {
-// 			switch(c) {
-// 				case 'l':
-// 					j = 4;
-// 					break;
-// 				case 'u':
-// 					if (j == 4)
-// 						serwrite_uint32(va_arg(args, uint32_t));
-// 					else
-// 						serwrite_uint16(va_arg(args, uint16_t));
-// 					j = 0;
-// 					break;
-// 				case 'd':
-// 					if (j == 4)
-// 						serwrite_int32(va_arg(args, int32_t));
-// 					else
-// 						serwrite_int16(va_arg(args, int16_t));
-// 					j = 0;
-// 					break;
-// 				case 'p':
-// 				case 'x':
-// 					serial_writestr_P(str_ox);
-// 					if (j == 4)
-// 						serwrite_hex32(va_arg(args, uint32_t));
-// 					else
-// 						serwrite_hex16(va_arg(args, uint16_t));
-// 					j = 0;
-// 					break;
-// 				case 'c':
-// 					serial_writechar(va_arg(args, uint16_t));
-// 					j = 0;
-// 					break;
-// 				case 's':
-// 					serial_writestr(va_arg(args, uint8_t *));
-// 					j = 0;
-// 					break;
-// 				default:
-// 					serial_writechar(c);
-// 					j = 0;
-// 					break;
-// 			}
-// 		}
-// 		else {
-// 			if (c == '%') {
-// 				j = 2;
-// 			}
-// 			else {
-// 				serial_writechar(c);
-// 			}
-// 		}
-// 	}
-// 	va_end(args);
-// }
 
 /** \brief Simplified printf
 	\param format pointer to output format specifier string stored in FLASH.
--- sersendf.h
+++ sersendf.h
@@ -5,7 +5,6 @@
 
 
 // No __attribute__ ((format (printf, 1, 2)) here because %q isn't supported.
-void sersendf(char *format, ...);
 void sersendf_P(PGM_P format_P, ...);
 
 #endif	/* _SERSENDF_H */
