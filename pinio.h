d739a2d456f158a4033a81176506e145e62020bc
Markus Hitter <mah@jump-ing.de>
2015-07-23 19:22:37 +0200

    ARM: implement SET_INPUT() and READ().
    Tested and found to work. Excellent.

--- pinio.h
+++ pinio.h
@@ -55,12 +55,18 @@
     on AVR costs 5 clock cycles, this implementation works with 3 clock cycles.
     Always assuming a well working optimiser.
 
+    Reading and writing 12 bits would be sufficient. For reading we choose
+    an uint32_t anyways, because casting to a smaller size is free, while
+    doing the opposite requires zero-extending the value ("UXTH" in assembly).
+
     The macros here are a bit more complex, because arm-gcc lacks hardware
     support.
   */
   /// The bit-banding address.
   #define BITBAND(IO)      (IO ## _PORT + (MASK(IO ## _PIN) << 2))
 
+  /// Read a pin.
+  #define _READ(IO)        (*(volatile uint32_t *)BITBAND(IO))
   /// Write to a pin.
   #define _WRITE(IO, v)    do { if (v) { *(volatile uint16_t *)BITBAND(IO) = \
                                            MASK(IO ## _PIN); } \
@@ -73,6 +79,13 @@
     GPIO behavior. Peripherals using these pins may have to change this and
     should do so in their own context.
   */
+  /// Set pin as input.
+  #define _SET_INPUT(IO)   do { *(volatile uint32_t *)IO ## _IOREG = \
+                                  (IO ## _OUTPUT | IO_MODEMASK_REPEATER); \
+                                *(volatile uint16_t *) \
+                                  (IO ## _PORT + IO_DIR_OFFSET) &= \
+                                  ~MASK(IO ## _PIN); \
+                           } while (0)
   /// Set pin as output.
   #define _SET_OUTPUT(IO)  do { *(volatile uint32_t *)IO ## _IOREG = \
                                   IO ## _OUTPUT; \
