917343e7f54b732b13f2272f6078c87f59b2e2a2
Nico Tonnhofer <wurstnase.reprap@gmail.com>
2015-11-16 22:49:02 +0100

    STM32F411: remove mbed-stm32f4xx_hal.c/.h removed also HAL_IncTick()/HAL_GetTick()     SIZES          ARM...     stm32f411     FLASH  :  2252 bytes           1%     RAM    :   136 bytes           1%     EEPROM :     0 bytes           0%

--- mbed-stm32f4xx_hal_conf.h
+++ mbed-stm32f4xx_hal_conf.h
@@ -50,7 +50,7 @@
 /**
   * @brief This is the list of modules to be used in the HAL driver 
   */
-#define HAL_MODULE_ENABLED         
+//#define HAL_MODULE_ENABLED         
 //#define HAL_ADC_MODULE_ENABLED
 //#define HAL_CAN_MODULE_ENABLED
 //#define HAL_CRC_MODULE_ENABLED
