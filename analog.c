e4f2b8ff3f3318cd8b43f28f2fe89c1b80b55c7a
Nico Tonnhofer <wurstnase.reprap@gmail.com>
2015-11-21 15:26:57 +0100

    ARM: rename analog-arm.c to add more ARMs

--- analog.c
+++ analog.c
@@ -35,7 +35,7 @@ static uint8_t adc_channel[NUM_TEMP_SENSORS] = {
 
 #define TEACUP_C_INCLUDE
 #include "analog-avr.c"
-#include "analog-arm.c"
+#include "analog-arm_lpc11xx.c"
 #undef TEACUP_C_INCLUDE
 
 // No common code so far.
