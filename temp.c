2291642456ec2c91ca683106e6aeced15f636e0e
Phil Hord <phil.hord@technicolor.com>
2016-04-14 18:20:41 -0400

    Expand thermistortable to include precomputed slope
    Save a division at runtime by pre-calculating the slope between each
    pair of adjacent thermistortable values.  Since we use the larger value
    each time, save the slope between two values A and B in the table
    with the B data.  Therefore the slope is that between each value and
    its predecessor in the list.
    
    Store this new value in the third element of the now 3-integers-wide
    array which makes up the table. Use fixed-point 6.10 format to store
    the slope.  This is almost too narrow for some slopes and maybe it
    should be changed to 8.8 fixed-point. In practice this presents a
    loss in accuracy, but it is still significantly better than the
    previous fixed-sample-size table production method. In particular no
    provision is made to handle values which scale over 65535, and it
    seems we should at least warn about this if not simply fail before
    letting the user go off compiling his code.
    
    Add a new flag TEMPTABLE_FORMAT and define it as 1 to tell the code
    that we are using this new and incompatible format.  This lets us
    tolerate old hand-crafted thermistor tables by keeping the slower
    algorithm in case one is still used.  New thermistor tables should be
    defined with this new format and with the FORMAT define set accordingly.
    
    With the default 25 samples this adds 100 bytes to the flash image for
    the thermistortable storage for two different thermistors.  But the
    code is simplified and saves me 134 bytes in the bargain for a net
    decrease in flash size of 34 bytes.

--- temp.c
+++ temp.c
@@ -196,28 +196,40 @@ static uint16_t temp_table_lookup(uint16_t temp, uint8_t sensor) {
         sersendf_P(PSTR("pin:%d Raw ADC:%d table entry: %d"),
                    temp_sensors[sensor].temp_pin, temp, j);
 
-      // Wikipedia's example linear interpolation formula.
-      // y = ((x - x₀)y₁ + (x₁-x)y₀) / (x₁ - x₀)
-      // y = temp
-      // x = ADC reading
-      // x₀= temptable[j-1][0]
-      // x₁= temptable[j][0]
-      // y₀= temptable[j-1][1]
-      // y₁= temptable[j][1]
-      temp = (
-      // ((x - x₀)y₁
-        ((uint32_t)temp - pgm_read_word(&(temptable[table_num][j-1][0]))) *
-                          pgm_read_word(&(temptable[table_num][j][1]))
-      //             +
-        +
-      //               (x₁-x)y₀)
-        (pgm_read_word(&(temptable[table_num][j][0])) - (uint32_t)temp) *
-          pgm_read_word(&(temptable[table_num][j - 1][1])))
-      //                        /
-        /
-      //                          (x₁ - x₀)
-        (pgm_read_word(&(temptable[table_num][j][0])) -
-         pgm_read_word(&(temptable[table_num][j - 1][0])));
+      #if (TEMPTABLE_FORMAT == 0)
+        // Wikipedia's example linear interpolation formula.
+        // y = ((x - x₀)y₁ + (x₁-x)y₀) / (x₁ - x₀)
+        // y = temp
+        // x = ADC reading
+        // x₀= temptable[j-1][0]
+        // x₁= temptable[j][0]
+        // y₀= temptable[j-1][1]
+        // y₁= temptable[j][1]
+        temp = (
+        // ((x - x₀)y₁
+          ((uint32_t)temp - pgm_read_word(&(temptable[table_num][j-1][0]))) *
+                            pgm_read_word(&(temptable[table_num][j][1]))
+        //             +
+          +
+        //               (x₁-x)y₀)
+          (pgm_read_word(&(temptable[table_num][j][0])) - (uint32_t)temp) *
+            pgm_read_word(&(temptable[table_num][j - 1][1])))
+        //                        /
+          /
+        //                          (x₁ - x₀)
+          (pgm_read_word(&(temptable[table_num][j][0])) -
+           pgm_read_word(&(temptable[table_num][j - 1][0])));
+        #elif (TEMPTABLE_FORMAT == 1)
+          // Linear interpolation using pre-computed slope.
+          // y = y₁ - (x - x₁) * d₁
+          #define X1 pgm_read_word(&(temptable[table_num][j][0]))
+          #define Y1 pgm_read_word(&(temptable[table_num][j][1]))
+          #define D1 pgm_read_word(&(temptable[table_num][j][2]))
+
+          temp = Y1 - ((((int32_t)temp - X1) * D1 + (1<<7)) >> 8);
+        #else
+          #error "temptable format unrecognized"
+        #endif
 
       if (DEBUG_PID && (debug_flags & DEBUG_PID))
         sersendf_P(PSTR(" temp:%d.%d"), temp / 4, (temp % 4) * 25);
