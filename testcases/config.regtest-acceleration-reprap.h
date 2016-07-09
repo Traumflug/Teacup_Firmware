76efe06284f432c812319d4813da6c818ebdcf1c
Markus Hitter <mah@jump-ing.de>
2016-07-09 13:48:56 +0200

    Regressiontests: add a test for ACCELERATION_REPRAP.

---
+++ testcases/config.regtest-acceleration-reprap.h
@@ -0,0 +1,10 @@
+
+// Configuration for controller board.
+#include "../config/board.ramps-v1.3.h"
+
+// Configuration for printer board.
+#include "../config/printer.mendel.h"
+
+#undef ACCELERATION_RAMPING
+#undef ACCELERATION_TEMPORAL
+#define ACCELERATION_REPRAP
