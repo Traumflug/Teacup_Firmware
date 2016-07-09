0555299717e4d78fbee79037fa7514c871f181ed
Markus Hitter <mah@jump-ing.de>
2016-07-09 13:53:16 +0200

    Regressiontests: add test for ACCELERATION_TEMPORAL.

---
+++ testcases/config.regtest-acceleration-temporal.h
@@ -0,0 +1,11 @@
+
+// Configuration for controller board.
+#include "../config/board.ramps-v1.3.h"
+
+// Configuration for printer board.
+#include "../config/printer.mendel.h"
+
+#undef ACCELERATION_REPRAP
+#undef ACCELERATION_RAMPING
+#undef LOOKAHEAD
+#define ACCELERATION_TEMPORAL
