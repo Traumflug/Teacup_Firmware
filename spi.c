3072279de18b90d260c90596ee2f530ef7b03348
Wurstnase <wurstnase.reprap@gmail.com>
2016-09-17 21:23:47 +0200

    ARM: split out spi-avr.c from spi.c

--- spi.c
+++ spi.c
@@ -1,4 +1,3 @@
-
 /** \file
   \brief SPI subsystem
 
@@ -11,36 +10,16 @@
   happens before reading and writing, data exchange its self is the same for
   each device, then.
 */
+
 #include "spi.h"
 
 #ifdef SPI
 
-/** Initialise SPI subsystem.
-
-  Code copied from ATmega164/324/644/1284 data sheet, section 18.2, page 160,
-  or moved here from mendel.c.
-*/
-void spi_init() {
-
-  // Set SCK (clock) and MOSI line to output, ie. set USART in master mode.
-  SET_OUTPUT(SCK);
-  SET_OUTPUT(MOSI);
-  SET_INPUT(MISO);
-  // SS must be set as output to disconnect it from the SPI subsystem.
-  // Too bad if something else tries to use this pin as digital input.
-  // See ATmega164/324/644/1284 data sheet, section 18.3.2, page 162.
-  // Not written there: this must apparently be done before setting the SPRC
-  // register, else future R/W-operations may hang.
-  SET_OUTPUT(SS);
-
-  #ifdef SPI_2X
-    SPSR = 0x01;
-  #else
-    SPSR = 0x00;
-  #endif
-
-  // This sets the whole SPRC register.
-  spi_speed_100_400();
-}
+#define TEACUP_C_INCLUDE
+#include "spi-avr.c"
+// Each ARM needs it's own file
+// #include "spi-arm_lpc11xx.c"
+// #include "spi-arm_stm32f4xx.c"
+#undef TEACUP_C_INCLUDE
 
 #endif /* SPI */
