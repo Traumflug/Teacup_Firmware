85fc7f86f4a4353406573dee2700582cb30cd7c9
Markus Hitter <mah@jump-ing.de>
2016-11-19 10:24:51 +0100

    Teacup_Firmware.ino: add a hint to Configtool.

--- Teacup_Firmware.ino
+++ Teacup_Firmware.ino
@@ -1,4 +1,9 @@
 
 /*
-	The existence of this file makes sources buildable with the Arduino IDE.
+  The existence of this file makes sources buildable with the Arduino IDE.
+
+  While Teacup can be built and uploaded using Arduino IDE, please consider
+  using Configtool for this task. Configtool not only allows printer
+  configuration in a GUI, it also builds smaller and faster binaries. Build
+  and upload functions can be found in the main menu there.
 */
