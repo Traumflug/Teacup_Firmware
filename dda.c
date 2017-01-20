1dbd030cdd53bad91e85d3723b76d71ec2979456
Phil Hord <phord@purestorage.com>
2017-01-20 12:58:39 -0800

    fixup: de-overflow accel calculations

--- dda.c
+++ dda.c
@@ -81,10 +81,10 @@ static const axes_uint32_t PROGMEM c0_P = {
 #define ACCEL_P_SHIFT 24
 // TODO: ASSERT(rampup_steps_to_vmax < 1<<(32-ACCEL_P_SHIFT))
 static const axes_uint32_t PROGMEM accel_P = {
-  (uint32_t)((((double)ACCELERATION * STEPS_PER_M_X) *(2<<ACCEL_P_SHIFT)) * QUANTUM / F_CPU * QUANTUM / F_CPU / 1000 + 1)/2,
-  (uint32_t)((((double)ACCELERATION * STEPS_PER_M_Y) *(2<<ACCEL_P_SHIFT)) * QUANTUM / F_CPU * QUANTUM / F_CPU / 1000 + 1)/2,
-  (uint32_t)((((double)ACCELERATION * STEPS_PER_M_Z) *(2<<ACCEL_P_SHIFT)) * QUANTUM / F_CPU * QUANTUM / F_CPU / 1000 + 1)/2,
-  (uint32_t)((((double)ACCELERATION * STEPS_PER_M_E) *(2<<ACCEL_P_SHIFT)) * QUANTUM / F_CPU * QUANTUM / F_CPU / 1000 + 1)/2
+  (uint32_t)((((double)ACCELERATION * STEPS_PER_M_X) *(2ULL<<ACCEL_P_SHIFT)) * QUANTUM / F_CPU * QUANTUM / F_CPU / 1000 + 1)/2,
+  (uint32_t)((((double)ACCELERATION * STEPS_PER_M_Y) *(2ULL<<ACCEL_P_SHIFT)) * QUANTUM / F_CPU * QUANTUM / F_CPU / 1000 + 1)/2,
+  (uint32_t)((((double)ACCELERATION * STEPS_PER_M_Z) *(2ULL<<ACCEL_P_SHIFT)) * QUANTUM / F_CPU * QUANTUM / F_CPU / 1000 + 1)/2,
+  (uint32_t)((((double)ACCELERATION * STEPS_PER_M_E) *(2ULL<<ACCEL_P_SHIFT)) * QUANTUM / F_CPU * QUANTUM / F_CPU / 1000 + 1)/2
 };
 
 /*! Set the direction of the 'n' axis
